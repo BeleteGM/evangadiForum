c2b401130fc36d0f182dbefc5092b0873e040e73
Belete Misgan Mengie <154594574+BeleteGM@users.noreply.github.com>
2024-03-26 14:34:48 -0900

    second commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ dotenv.config();
 const app = express();
 app.use(cors())
 app.use(bodyparser.urlencoded({ extended: true }));
-const port = 5000;
+const port = 3306;
 const dbconnection = require("./db/dbconnection");
 
 const userRoute = require("./route/userRoute");
